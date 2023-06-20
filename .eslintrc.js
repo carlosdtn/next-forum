dcdc53f5458b8d3fef4ab1286e6c5f34739d05e7
Carlos Tarmeno <carlos-100-800@hotmail.com>
2023-06-20 17:31:36 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+const RULES = {
+  OFF: "off",
+  ERROR: "error",
+  WARN: "warn",
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "next/core-web-vitals",
+  rules: {
+    "react/prop-types": RULES.OFF,
+  },
+};
