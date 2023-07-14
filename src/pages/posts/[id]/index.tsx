adfad36b12e7373b89ffc53bcb855166cee2a21f
Railly <railly_2009@hotmail.com>
2023-07-14 15:50:21 -0500

    add delete post

--- src/pages/posts/[id]/index.tsx
+++ src/pages/posts/[id]/index.tsx
@@ -2,12 +2,16 @@ import { IPost } from "@/components/cards/post-card"
 import { MOCK_POSTS } from "@/lib/mock"
 import { useRouter } from "next/router"
 import Image from "next/image"
-import { useEffect, useState } from "react"
+import { Fragment, useEffect, useState } from "react"
+import { TrashIcon, XIcon } from "lucide-react"
+import { Dialog, Transition } from "@headlessui/react"
 
 const Post = () => {
   const [isFavorite, setIsFavorite] = useState(false)
   const [isLiked, setIsLiked] = useState(0)
   const [post, setPost] = useState<IPost | null>(null)
+  const [isOpen, setIsOpen] = useState(false)
+  const router = useRouter()
 
   const id = useRouter().query.id
   console.log({ id })
@@ -30,9 +34,26 @@ const Post = () => {
   if (post) {
     const days = Math.floor(
       (new Date().getTime() - new Date(post?.fechaCreacion).getTime()) /
-        (1000 * 60 * 60 * 24)
+      (1000 * 60 * 60 * 24)
     )
     timeAgo = rtf.format(-days, "day")
+  }
+
+  const deletePost = async (id: number) => {
+    const token = window.localStorage.getItem("token")
+    const authorizationHeader = token ? `Bearer ${JSON.parse(token)}` : ''
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_NGROK_URL}/Posts/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: authorizationHeader,
+        },
+      }
+    )
+    const post = await res.json()
+    console.log({ post })
+    if (post.isSuccess) router.push(`/`)
   }
 
   return (
@@ -118,28 +139,109 @@ const Post = () => {
             </p>
           </button>
         </div>
-        <button
-          onClick={() => {
-            setIsFavorite(!isFavorite)
-          }}
-          title="Favorite this post"
-          aria-label="Favorite this post"
-          className="flex gap-2 rounded bg-emerald-500 px-2 py-1 shadow-lg hover:opacity-80 sm:p-2 relative right-6"
-        >
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth="0"
-            viewBox="0 0 1024 1024"
-            className={isFavorite ? "text-pink-600" : "text-white"}
-            height="22"
-            width="22"
-            xmlns="http://www.w3.org/2000/svg"
+        <div className="flex gap-4">
+          <button
+            onClick={() => {
+              setIsFavorite(!isFavorite)
+            }}
+            title="Favorite this post"
+            aria-label="Favorite this post"
+            className="flex gap-2 rounded bg-emerald-500 px-2 py-1 shadow-lg hover:opacity-80 sm:p-2 relative right-6"
           >
-            <path d="M923 283.6a260.04 260.04 0 0 0-56.9-82.8 264.4 264.4 0 0 0-84-55.5A265.34 265.34 0 0 0 679.7 125c-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5a258.44 258.44 0 0 0-56.9 82.8c-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3.1-35.3-7-69.6-20.9-101.9z"></path>
-          </svg>
-        </button>
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 1024 1024"
+              className={isFavorite ? "text-pink-600" : "text-white"}
+              height="22"
+              width="22"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M923 283.6a260.04 260.04 0 0 0-56.9-82.8 264.4 264.4 0 0 0-84-55.5A265.34 265.34 0 0 0 679.7 125c-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5a258.44 258.44 0 0 0-56.9 82.8c-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3.1-35.3-7-69.6-20.9-101.9z"></path>
+            </svg>
+          </button>
+          <button
+            onClick={() => setIsOpen(true)}
+            title="Delete this post"
+            aria-label="Delete this post"
+            className="flex gap-2 rounded bg-red-500 px-2 py-1 shadow-lg hover:opacity-80 sm:p-2 relative right-6"
+          >
+            <TrashIcon fill="white" stroke="white" />
+          </button>
+        </div>
       </div>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={() => setIsOpen(false)}
+        >
+          <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-xl p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <button
+                  type="button"
+                  className="fixed top-0 left-0 p-1 mt-3 ml-3 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-full hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <XIcon className="w-5 h-5 fill-blue-500" />
+                </button>
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-center text-gray-900"
+                >
+                  ¿Estás seguro de eliminar este post?
+                </Dialog.Title>
+                <div className="mt-8"></div>
+                <Dialog.Description>
+                  <div className="flex flex-row justify-center gap-4">
+                    <button
+                      onClick={() => setIsOpen(false)}
+                      className="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-gray-700 border border-transparent rounded-md shadow-sm hover:bg-gray-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-gray-500"
+                    >
+                      Cancelar
+                    </button>
+                    <button
+                      onClick={() => deletePost(post?.id)}
+                      className="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-red-500 border border-transparent rounded-md shadow-sm hover:bg-red-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500"
+                    >
+                      Eliminar
+                    </button>
+                  </div>
+                </Dialog.Description>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
   )
 }
